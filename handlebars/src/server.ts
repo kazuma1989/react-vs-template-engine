b0dcae41c0e5ec9335cb79f819a61404131217d1
kazuma1989 <kazuma.ebina@quipper.com>
2021-12-05 20:36:46 +0900

    feat: HTML template

--- handlebars/src/server.ts
+++ handlebars/src/server.ts
@@ -1,4 +1,6 @@
+import fs from "fs/promises"
 import Handlebars from "handlebars"
+import path from "path"
 import polka from "polka"
 
 const app = polka().listen(3000, (err: unknown) => {
@@ -7,8 +9,12 @@ const app = polka().listen(3000, (err: unknown) => {
   console.log(`> Running on localhost:3000`)
 })
 
-app.get("/users/:id", (req, res) => {
-  const template = Handlebars.compile("{{title}} spends {{calc}}")
+app.get("/users/:id", async (req, res) => {
+  const source = await fs
+    .readFile(path.resolve(__dirname, "./template.hbs"))
+    .then((buffer) => buffer.toString())
+
+  const template = Handlebars.compile(source)
 
   const view = {
     title: req.params.id,
@@ -17,5 +23,6 @@ app.get("/users/:id", (req, res) => {
 
   const output = template(view)
 
+  res.setHeader("Content-Type", "text/html")
   res.end(output)
 })
