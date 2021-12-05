969866d65c91d79d7a4ebb0ab940d0db65f4bdfa
kazuma1989 <kazuma.ebina@quipper.com>
2021-12-05 22:27:53 +0900

    refactor: Appを切り出した

---
+++ react/src/App.tsx
@@ -0,0 +1,74 @@
+import { useDeferredValue, useState } from "react"
+import useSWR from "swr"
+
+interface Todo {
+  completed: boolean
+  id: number
+  title: string
+  userId: number
+}
+
+export function App() {
+  const [currentPage, setCurrentPage] = useState(1)
+  const deferredPage = useDeferredValue(currentPage)
+  const pending = currentPage !== deferredPage
+
+  const todos: Todo[] = useSWR(
+    `https://jsonplaceholder.typicode.com/todos?_limit=10&_page=${deferredPage}`
+  ).data
+
+  return (
+    <div>
+      <h1>React 18 Example</h1>
+
+      <div>
+        {[1, 2, 3, 4, 5].map((page) => {
+          const current = page === currentPage
+
+          return (
+            <button
+              key={page}
+              type="button"
+              onClick={() => {
+                setCurrentPage(page)
+              }}
+              style={{
+                backgroundColor: current ? "var(--focus)" : undefined,
+              }}
+            >
+              {page}
+            </button>
+          )
+        })}
+      </div>
+
+      <progress style={{ visibility: pending ? "visible" : "hidden" }}>
+        Loading...
+      </progress>
+
+      <table
+        style={{
+          tableLayout: "auto",
+        }}
+      >
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>TITLE</th>
+            <th>COMPLETED</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {todos.map(({ id, title, completed }) => (
+            <tr key={id}>
+              <td>{id}</td>
+              <td>{title}</td>
+              <td>{completed ? "YES" : "NO"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,6 +1,7 @@
-import { StrictMode, Suspense, useDeferredValue, useState } from "react"
+import { StrictMode, Suspense } from "react"
 import { createRoot } from "react-dom"
-import useSWR, { SWRConfig } from "swr"
+import { SWRConfig } from "swr"
+import { App } from "./App"
 
 createRoot(globalThis.document.getElementById("root")!).render(
   <StrictMode>
@@ -12,7 +13,7 @@ createRoot(globalThis.document.getElementById("root")!).render(
         dedupingInterval: 86400_000,
         suspense: true,
         fetcher: (url) =>
-          delay(1_000).then(() => fetch(url).then((r) => r.json())),
+          fakeDelay(1_000).then(() => fetch(url).then((r) => r.json())),
       }}
     >
       <Suspense fallback={<progress>Loading...</progress>}>
@@ -22,80 +23,8 @@ createRoot(globalThis.document.getElementById("root")!).render(
   </StrictMode>
 )
 
-function delay(ms: number) {
+function fakeDelay(ms: number) {
   return new Promise<void>((resolve) => {
     setTimeout(resolve, ms)
   })
-}
-
-interface Todo {
-  completed: boolean
-  id: number
-  title: string
-  userId: number
-}
-
-function App() {
-  const [currentPage, setCurrentPage] = useState(1)
-  const deferredPage = useDeferredValue(currentPage)
-  const pending = currentPage !== deferredPage
-
-  const todos: Todo[] = useSWR(
-    `https://jsonplaceholder.typicode.com/todos?_limit=10&_page=${deferredPage}`
-  ).data
-
-  return (
-    <div>
-      <h1>React 18 Example</h1>
-
-      <div>
-        {[1, 2, 3, 4, 5].map((page) => {
-          const current = page === currentPage
-
-          return (
-            <button
-              key={page}
-              type="button"
-              onClick={() => {
-                setCurrentPage(page)
-              }}
-              style={{
-                backgroundColor: current ? "var(--focus)" : undefined,
-              }}
-            >
-              {page}
-            </button>
-          )
-        })}
-      </div>
-
-      <progress style={{ visibility: pending ? "visible" : "hidden" }}>
-        Loading...
-      </progress>
-
-      <table
-        style={{
-          tableLayout: "auto",
-        }}
-      >
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>TITLE</th>
-            <th>COMPLETED</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {todos.map(({ id, title, completed }) => (
-            <tr key={id}>
-              <td>{id}</td>
-              <td>{title}</td>
-              <td>{completed ? "YES" : "NO"}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
 }
