605dfd952358e0c50f3a86e91d6f67ff4265e970
kazuma1989 <kazuma.ebina@quipper.com>
2021-12-12 11:43:45 +0900

    refactor: template先読み＆キャッシュ

--- handlebars/src/app.ts
+++ handlebars/src/app.ts
@@ -6,16 +6,17 @@ import { Polka } from "polka"
 
 type Query = Parameters<Parameters<Polka["get"]>[1]>[0]["query"]
 
-export async function app(query: Query): Promise<string> {
-  const template = await fs
-    .readFile(path.resolve(__dirname, "./index.hbs"), "utf-8")
-    .then(Handlebars.compile)
+const template$ = fs
+  .readFile(path.resolve(__dirname, "./index.hbs"), "utf-8")
+  .then(Handlebars.compile)
 
+export async function app(query: Query): Promise<string> {
   const currentPage = parseInt(query.page as string) || 1
   const todos = await fetch(
     `https://jsonplaceholder.typicode.com/todos?_limit=10&_page=${currentPage}`
   ).then((r) => r.json())
 
+  const template = await template$
   return template({
     pages: [1, 2, 3, 4, 5].map((page) => ({
       page,
