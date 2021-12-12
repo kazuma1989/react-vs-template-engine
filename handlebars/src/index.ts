820de6c7fdf35f05e0235903e8991c2f8617ca40
kazuma1989 <kazuma.ebina@quipper.com>
2021-12-12 11:16:07 +0900

    refactor: キャッシュ効率より処理の読みやすさを重視
    説明用コードなので。

--- handlebars/src/index.ts
+++ handlebars/src/index.ts
@@ -4,46 +4,46 @@ import fetch from "node-fetch"
 import path from "path"
 import polka from "polka"
 
-const port = 5006
-const app = polka().listen(port, (err: unknown) => {
-  if (err) throw err
+run()
 
-  console.log(`> Running on localhost:${port}`)
-})
-
-const setup$ = Promise.all([
-  fs
+async function run(): Promise<void> {
+  await fs
     .readFile(path.resolve(__dirname, "./layout.hbs"), "utf-8")
-    .then((source) => Handlebars.registerPartial("layout", source)),
-])
+    .then((source) => Handlebars.registerPartial("layout", source))
 
-const template$ = fs
-  .readFile(path.resolve(__dirname, "./index.hbs"), "utf-8")
-  .then(Handlebars.compile)
+  const app = polka()
 
-app.get("/", async (req, res) => {
-  await setup$
+  app.get("/", async (req, res) => {
+    const template = await fs
+      .readFile(path.resolve(__dirname, "./index.hbs"), "utf-8")
+      .then(Handlebars.compile)
 
-  const template = await template$
+    await fakeDelay(1_000)
 
-  await fakeDelay(1_000)
+    const currentPage = parseInt(req.query.page as string) || 1
+    const todos = await fetch(
+      `https://jsonplaceholder.typicode.com/todos?_limit=10&_page=${currentPage}`
+    ).then((r) => r.json())
 
-  const currentPage = parseInt(req.query.page as string) || 1
-  const todos = await fetch(
-    `https://jsonplaceholder.typicode.com/todos?_limit=10&_page=${currentPage}`
-  ).then((r) => r.json())
+    const output = template({
+      pages: [1, 2, 3, 4, 5].map((page) => ({
+        page,
+        current: page === currentPage,
+      })),
+      todos,
+    })
 
-  const output = template({
-    pages: [1, 2, 3, 4, 5].map((page) => ({
-      page,
-      current: page === currentPage,
-    })),
-    todos,
+    res.setHeader("Content-Type", "text/html")
+    res.end(output)
   })
 
-  res.setHeader("Content-Type", "text/html")
-  res.end(output)
-})
+  const port = 5006
+  app.listen(port, (err: unknown) => {
+    if (err) throw err
+
+    console.log(`> Running on localhost:${port}`)
+  })
+}
 
 function fakeDelay(ms: number) {
   return new Promise<void>((resolve) => {
